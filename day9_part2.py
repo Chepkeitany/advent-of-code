7b0dd0a44d47feebdb26f93346575bcb754d1d65
Anne Chepkeitany <rerimoianne.4@gmail.com>
2023-12-09 05:53:18 +0000

    Create day9_part2.py

---
+++ day9_part2.py
@@ -0,0 +1,43 @@
+def sum_next_number_in_sequence(sequences):
+    sum = 0
+
+    for sequence in sequences:
+        sequence = sequence.split(" ")
+        sequence = [int(i) for i in sequence]
+
+        # Make a new sequence from the difference at each step of your history
+        # Once all of the values in your latest sequence are zeroes, you can extrapolate what the next value of the original history should be
+        # work out the next value in each sequence from the bottom up:
+        all_zeroes = False
+        temp_sequences = [sequence]
+        while not all_zeroes:
+            new_sequence = []
+            for i in range(len(sequence) - 1):
+                new_sequence.append(int(sequence[i + 1]) - int(sequence[i]))
+
+            temp_sequences.append(new_sequence)
+
+            # If that sequence is not all zeroes, repeat this process, using the sequence you just generated as the input sequence
+            if new_sequence[len(new_sequence) - 1] == 0:
+                all_zeroes = True
+            else:
+                sequence = new_sequence
+
+        # Starting from last sequence, work out the next value in each sequence from the bottom up
+        # by extrapolating backwards i.e filling in the first values for each previous sequence
+        next_number = 0
+        for i in range(len(temp_sequences) - 1, -1, -1):
+            temp_sequence = temp_sequences[i]
+            # print(temp_sequence[0] - next_number)
+            next_number = temp_sequence[0] - next_number
+        
+        # print(next_number)
+        sum += next_number
+    return sum
+
+
+with open("day9_all.txt") as file:
+    lines = file.read().split("\n")
+
+result = sum_next_number_in_sequence(lines)
+print(result)
