b0d96a7bbeb72bed5add53f583bd6f8b25fc1a74
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-05 14:16:55 +0000

    Add solution for day2 part2

--- 2015/day2.py
+++ 2015/day2.py
@@ -17,15 +17,42 @@ def calculate_total_square_feet(input):
 
     return total_square_feet
 
+def calculate_smallest_perimeter(input):
+    ribbon_length = 0
+    for line in input:
+        # Extract l, w, h
+        lwh = line.split('x')
+        l,w,h = [int(i) for i in lwh]
+        l_w = l + w
+        w_h = w + h
+        h_l = h + l
+
+        smallest_side = min(l_w, w_h, h_l)
+
+        ribbon_length += 2 * smallest_side
+        ribbon_length += l * w * h
+    return ribbon_length
+
 # Part 1
 # Sample test cases
 file = open("day2_test.txt")
 content = file.read().splitlines()
 
-assert calculate_total_square_feet(content) == 101, "Failed on sample input"
+assert calculate_total_square_feet(content) == 101,     "Failed on sample input"
 
 file = open("day2_all.txt")
 content = file.read().splitlines()
 assert calculate_total_square_feet(content) == 1598415, "Failed on main input"
+
+# Part 2
+# Sample test cases
+file = open("day2_test.txt")
+content = file.read().splitlines()
+
+assert calculate_smallest_perimeter(content) == 48,     "Failed on sample input"
+
+file = open("day2_all.txt")
+content = file.read().splitlines()
+assert calculate_smallest_perimeter(content) == 3812909, "Failed on main input"
 
 print("All tests passed!")
