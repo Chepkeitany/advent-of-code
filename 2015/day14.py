e2126bf2175dbd90551db33e8f60b2040ead8fda
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-26 06:04:36 +0000

    Rename function name

--- 2015/day14.py
+++ 2015/day14.py
@@ -4,7 +4,7 @@ Each reindeer flies at a certain speed then rests.
 '''
 
 
-def find_winning_reindeer(lines, time_elapsed):
+def find_winning_distance(lines, time_elapsed):
     '''Find the reindeer that has travelled the farthest distance after a given time has elapsed'''
     fastest_distance = 0
     for line in lines:
@@ -38,13 +38,13 @@ def find_winning_reindeer(lines, time_elapsed):
 with open("day14_test.txt", encoding="utf-8") as file:
     content = file.read().splitlines()
 
-    assert find_winning_reindeer(
+    assert find_winning_distance(
         content, 1000) == 1120, "Failed on test input - part 1"
 
 with open("day14_all.txt", encoding="utf-8") as file:
     content = file.read().splitlines()
 
-    assert find_winning_reindeer(
+    assert find_winning_distance(
         content, 2503) == 2655, "Failed on main input - part1"
 
     print("All tests passed!")
